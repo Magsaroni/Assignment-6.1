504080a51f446ceafd663d0c0207721b82c465e8
Mags <144168396+Magsaroni@users.noreply.github.com>
2024-03-05 21:42:15 -0700

    Add files via upload

--- DoubleDigits.js
+++ DoubleDigits.js
@@ -42,3 +42,41 @@ var die = Math.random() * 55;
 //we will get a rounded whole number.
 return Math.ceil(die)
 }
+
+  // Get the first name entered.
+  function validateForm(){
+    var firstName = document.getElementById("fname").value;
+
+    // Get the last name entered.
+    var lastName = document.getElementById("Lname").value;
+
+    // Get the zip code entered.
+    var zipCode = document.getElementById("zip").value;
+
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+    console.log("Zip Code: " + zipCode);
+
+    var fullName = firstName + " " + lastName;
+    console.log("Full Name: " + fullName);
+
+    if (fullName.length > 20 || fullName.length == 1){
+        console.log("Invalid Name");
+        // Send an alert if what they entered is invalid.
+        alert("Please enter a name that is shorter than 20 characters.")
+
+        return false;
+    }
+    else if (zipCode.length != 5){
+        console.log("Invalid Zip Code");
+        alert("Zip code was not 5 digits. Please try again.")
+        return false;
+    }
+    else{
+        console.log("We are good to go!");
+        alert("Welcome, " + fullName + ". The secret word is Validation");
+        return false
+    }
+
+    return false;
+}
